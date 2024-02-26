c846413502e0de6367ee4b64f5c935564e58da42
Tyler Chen <tyler.chen@userful.com>
2024-02-26 10:16:42 -0700

    recreated project with android java

---
+++ flutter/example_8k/android/app/src/main/java/com/example/example_8k/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.example_8k;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- flutter/example_8k/android/app/src/main/kotlin/com/example/example_8k/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.example_8k
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
